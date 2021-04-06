6a4b284d2f0237b1493b4de39ec5dc73311e0b0c
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-06 02:18:04 -0300

    a

--- function/closure/6.js
+++ function/closure/6.js
@@ -1,10 +1,8 @@
 const obj1 = {};
 for (var v1 = 0; v1 < 3; v1++) {
-    obj1[v1] = (function (v2) {
-        return function() {
-            console.log(v2);
-        }
-    }(v1))
+    obj1[v1] = (function () {
+        console.log(this.v2);
+    }).bind({ v2: v1 });
 }
 obj1[0]();
 obj1[1]();
