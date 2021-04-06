d73862ed0fae7621d198cd7f7c712ea85e4491c5
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-06 00:01:53 -0300

    new

---
+++ function/new/1.js
@@ -0,0 +1,21 @@
+const person = { 
+    name: "Linus Torvalds",
+    city: "Helsinki",
+    year: 1969,
+    getAge() {
+        return (new Date()).getFullYear() - this.year
+    }
+}
+
+const person2 = { 
+    name: "Bill Gates",
+    city: "Seattle",
+    year: 1955,
+    getAge() {
+        return (new Date()).getFullYear() - this.year
+    }
+}
+
+
+console.log(person.getAge())
+console.log(person2.getAge())
