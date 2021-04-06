3b94f945c17fadd1d0147180b65630b1b5a6c4ef
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-06 00:15:29 -0300

    new

--- function/new/1.js
+++ function/new/1.js
@@ -1,21 +1,23 @@
-const person = { 
-    name: "Linus Torvalds",
-    city: "Helsinki",
-    year: 1969,
+const personPrototype = {
     getAge() {
-        return (new Date()).getFullYear() - this.year
+        return (new Date()).getFullYear() - this.year;
     }
-}
+};
 
-const person2 = { 
-    name: "Bill Gates",
-    city: "Seattle",
-    year: 1955,
-    getAge() {
-        return (new Date()).getFullYear() - this.year
-    }
-}
+const createPerson = (name, city, year) => {
+    const person = {
+        name,
+        city,
+        year
+    };
 
+    Object.setPrototypeOf(person, personPrototype);
+
+    return person;
+};
+
+const person = createPerson("Linus Torvalds", "Helsinki", 1969);
+const person2 = createPerson("Bill Gates", "Seattle", 1955);
 
 console.log(person.getAge())
 console.log(person2.getAge())
