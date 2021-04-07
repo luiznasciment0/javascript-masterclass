3a978041962a61a3e1904bb8011ecbdda6ec2819
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-07 00:54:24 -0300

    exercise

--- exercise.js
+++ exercise.js
@@ -8,26 +8,42 @@ const database = {
     createTable(command) {
         const regexp = /create table ([a-z]+) \((.+)\)/;
         const parsedStatement = command.match(regexp);
-        const tableName = parsedStatement[1];
+        let [, tableName, columns] = parsedStatement;
 
         this.tables[tableName] = {
             columns: {},
             data: []
         };
 
-        let columns = parsedStatement[2];
         columns = columns.split(',');
 
         for (let column of columns) {
             column = column.trim().split(" ")
-            const name = column[0]
-            const type = column[1]
+            const [name, type] = column
             this.tables[tableName].columns[name] = type
         }
+    },
+    insert(command) {
+        const regexp = /insert into ([a-z]+) \((.+)\) values \((.+)\)/;
+        const parsedStatement = command.match(regexp);
+        let [, tableName, columns, values] = parsedStatement;
+        columns = columns.split(', ')
+        values = values.split(', ')
+        let row = {}
+        for (let index = 0; index < columns.length; index++) {
+            const column = columns[index]
+            const value = values[index]
+            row[column] = value;
+        }
+        this.tables[tableName].data.push(row);
     },
     execute(statement) {
         if (statement.startsWith('create table')) {
             return this.createTable(statement)
+        }
+
+        if (statement.startsWith('insert')) {
+            return this.insert(statement)
         }
         
         const message = `Syntax error: '${statement}'`
@@ -37,7 +53,9 @@ const database = {
 
 try {
     database.execute('create table author (id number, name string, age number, city string, state string, country string)')
-    database.execute("select id, name from author");
+    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
 } catch (e) {
     console.log(e.message);
 }
