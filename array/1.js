1a217b33462ada10a7e54a5f3e9065d51f3cc5eb
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-06 22:51:05 -0300

    start array

---
+++ array/1.js
@@ -0,0 +1,6 @@
+const languages = ["Python", "C", "Java"];
+const languages2 = new Array("Python", "C", "Java");
+const a = new Array(10);
+console.log(languages);
+console.log(languages2.length);
+console.log(a);
---
+++ array/2.js
@@ -0,0 +1,10 @@
+const languages = [];
+languages[0] = 'Python';
+languages[10] = 'C';
+languages[20] = 'Java';
+
+console.log(languages);
+console.log(languages.length);
+delete languages[1];
+console.log(languages);
+console.log(languages.length);
