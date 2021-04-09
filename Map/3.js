8b134a66d5c30558432bbef3a9bc036331e21040
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-08 21:36:53 -0300

    a

--- Map/3.js
+++ Map/3.js
@@ -7,3 +7,5 @@ console.log(timeUnits.forEach((value, key) => {
 }));
 console.log(timeUnits.has("hour"));
 console.log(timeUnits.has("day"));
+console.log(timeUnits.get("day"));
+console.log(timeUnits.get("hour"));
