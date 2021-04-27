9cb8a55d91acb68cfd64da7407c53f356b33bdb6
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-27 01:24:05 -0300

    a

--- Classes/1.js
+++ Classes/1.js
@@ -23,3 +23,28 @@ console.log(square.calculateArea());
 const square2 = Square.fromArea(16);
 console.log(square2);
 console.log(square2.calculateArea());
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    calculateArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    toString() {
+        return `radius: ${this.radius}, area: ${this.calculateArea()}`;
+    }
+
+    static fromArea(area) {
+        return new Circle(Math.sqrt(area / Math.PI));
+    }
+}
+
+const circle = new Circle(10);
+console.log(circle.toString());
+console.log(circle.calculateArea());
+
+const circle2 = Circle.fromArea(314.1592653589793);
+console.log(circle2);
