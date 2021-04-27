f5c65504d332c751a65e22307b779f8a14fb8839
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-26 22:49:42 -0300

    spread

--- iterabelsiterators/1.js
+++ iterabelsiterators/1.js
@@ -1,15 +1,20 @@
-const languages = ["Fortran", "Lisp", "COBOL"];
+const classicLangs = ["Fortran", "Lisp", "COBOL"];
+const modernLangs = ["Python", "Ruby", "JavaScript"];
 
-for (let i = 0; i < languages.length; i++) {
-    console.log(languages[i])
+for (let i = 0; i < classicLangs.length; i++) {
+    console.log(classicLangs[i])
 }
 
-for (let i in languages) {
-    console.log(languages[i])
+for (let i in classicLangs) {
+    console.log(classicLangs[i])
 }
 
-languages.forEach(language => console.log(language))
+classicLangs.forEach(language => console.log(language))
 
-for (let language of languages) {
+for (let language of classicLangs) {
     console.log(language);
 }
+
+// const languages = classicLangs.concat(modernLangs)
+const languages = [...classicLangs, ...modernLangs]
+console.log('languages', languages)
