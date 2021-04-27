146a7748b02e98f90612739fb991f52b4c44d5c7
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-27 01:17:15 -0300

    a

--- Classes/1.js
+++ Classes/1.js
@@ -10,8 +10,16 @@ class Square {
     toString() {
         return `side: ${this.side}, area: ${this.calculateArea()}`
     }
+
+    static fromArea(area) {
+        return new Square(Math.sqrt(area));
+    }
 }
 const square = new Square(4);
 console.log(square);
 console.log(square.toString());
 console.log(square.calculateArea());
+
+const square2 = Square.fromArea(16);
+console.log(square2);
+console.log(square2.calculateArea());
