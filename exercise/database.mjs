7e76191a219234a0b85fcab53fbe0be01c37fb88
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-27 02:39:09 -0300

    modules

--- exercise/database.mjs
+++ exercise/database.mjs
@@ -1,33 +1,7 @@
-class DatabaseError {
-    constructor(statement, message) {
-        this.statement = statement;
-        this.message = message;
-    }
-}
+import { Parser } from './parser'
+import { DatabaseError } from './database-error'
 
-class Parser {
-    constructor() {
-        this.commands = new Map();
-        this.commands.set("createTable", /create table ([a-z]+) \((.+)\)/);
-        this.commands.set("insert", /insert into ([a-z]+) \((.+)\) values \((.+)\)/);
-        this.commands.set("select", /select (.+) from ([a-z]+)(?: where (.+))?/);
-        this.commands.set("delete", /delete from ([a-z]+)(?: where (.+))?/);
-    }
-
-    parse(statement) {
-        for (let [command, regexp] of this.commands) {
-            const parsedStatement = statement.match(regexp)
-            if (parsedStatement) {
-                return {
-                    command,
-                    parsedStatement
-                }
-            }
-        }
-    }
-}
-
-class Database {
+export class Database {
     constructor() {
         this.tables = {};
         this.parser = new Parser();
@@ -113,19 +87,3 @@ class Database {
         throw new DatabaseError(statement, message);
     }
 }
-
-try {
-    const database = new Database();
-    database.execute("create table author (id number, name string, age number, city string, state string, country string)")
-    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
-    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    database.execute("select name, age from author");
-    database.execute("select name, age from author where id = 1");
-    database.execute("delete from author where id = 2");
-    console.log(database.execute("select name, age from author"));
-} catch (e) {
-    console.log(e.message);
-}
-
-// console.log(JSON.stringify(database, undefined, " "))
---
+++ exercise/exercise.mjs
@@ -0,0 +1,17 @@
+import { Database } from './database'
+
+try {
+    const database = new Database();
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)")
+    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+    database.execute("select name, age from author");
+    database.execute("select name, age from author where id = 1");
+    database.execute("delete from author where id = 2");
+    console.log(database.execute("select name, age from author"));
+} catch (e) {
+    console.log(e.message);
+}
+
+// console.log(JSON.stringify(database, undefined, " "))
