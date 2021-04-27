cc25b6a88f98c6db5fd1d9f55fce6dd1490c8d67
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-27 00:16:28 -0300

    a

--- iterablesiterators/4.js
+++ iterablesiterators/4.js
@@ -23,3 +23,31 @@ console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
+
+function createIterable(...array) {
+    return {
+        [Symbol.iterator]() {
+            let index = 0;
+            return {
+                next() {
+                    if (index < array.length) {
+                        return {
+                            value: array[index++],
+                            done: false
+                        };
+                    } else {
+                        return {
+                            value: undefined,
+                            done: true
+                        };
+                    }
+                }
+            };
+        }
+    };
+}
+
+const iterable = createIterable("Fortran", "Lisp", "COBOL");
+for (let language of iterable) {
+    console.log(language)
+}
