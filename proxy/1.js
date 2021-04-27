f026a7ba7942512bb69a9c0e2dfbbb83faec916c
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-27 02:06:59 -0300

    delete

--- proxy/1.js
+++ proxy/1.js
@@ -4,6 +4,12 @@ function createArray() {
             target.length = target.length || 0;
             target.length++;
             target[key] = value;
+        },
+        deleteProperty(target, key) {
+            if (key in target) {
+                target.length--;
+                delete target[key];
+            }
         }
     });
 }
@@ -11,6 +17,13 @@ const languages = createArray();
 languages[0] = "Python";
 languages[1] = "Ruby";
 languages[2] = "JavaScript";
+
+console.log(languages);
+console.log(languages.length);
+
+delete languages[1];
+delete languages[2];
+delete languages[3];
 
 console.log(languages);
 console.log(languages.length);
