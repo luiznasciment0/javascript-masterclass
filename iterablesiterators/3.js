b4e2d4d16fcb8af138700caf686aa5aa54afb2d4
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-26 23:19:18 -0300

    a

--- iterablesiterators/3.js
+++ iterablesiterators/3.js
@@ -4,3 +4,10 @@ console.log(iterator.next())
 console.log(iterator.next())
 console.log(iterator.next())
 console.log(iterator.next())
+
+const language = "Fortran";
+const iterator2 = language[Symbol.iterator]();
+console.log(iterator2.next())
+console.log(iterator2.next())
+console.log(iterator2.next())
+console.log(iterator2.next())
