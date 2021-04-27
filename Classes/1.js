a8b1208f82e97a590717de669fe5b93eaf25e84a
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-27 01:13:34 -0300

    entenddendo classe

---
+++ Classes/1.js
@@ -0,0 +1,17 @@
+class Square {
+    constructor(side) {
+        this.side = side;
+    }
+
+    calculateArea() {
+        return Math.pow(this.side, 2)
+    }
+    
+    toString() {
+        return `side: ${this.side}`
+    }
+}
+const square = new Square(4);
+console.log(square);
+console.log(square.toString());
+console.log(square.calculateArea());
