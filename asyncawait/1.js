162b2b49576f28f85a1420e252c7315c428f90c4
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-28 02:33:15 -0300

    a

--- asyncawait/1.js
+++ asyncawait/1.js
@@ -8,8 +8,12 @@ function sum(a, b) {
 }
 
 (async function() {
-    const a = await sum(2, 2);
-    const b = await sum(4, 4);
-    const result = await sum(a, b);
-    console.log(result);
+    try {
+        const a = await sum(2);
+        const b = await sum(4, 4);
+        const result = await sum(a, b);
+        console.log(result);
+    } catch (e) {
+        console.log(e);
+    }
 })();
