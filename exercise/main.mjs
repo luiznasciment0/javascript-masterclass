6fdccdb4ac78d3c1a6ece5fb9df04c45664313c6
luiznasciment0 <luizp.nascimento@outlook.com>
2021-04-28 02:47:00 -0300

    a

--- exercise/main.mjs
+++ exercise/main.mjs
@@ -1,21 +1,17 @@
 import { Database } from './database'
 
-const database = new Database();
-database.execute("create table author (id number, name string, age number, city string, state string, country string)").then(() => {
-   return Promise.all([
-        database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
-        database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
-        database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)"),
-   ]).then(() => {
-        return database.execute("select name, age from author").then((result) => {
-            console.log(result);
-        });
-   })
-}).catch(e => console.log(e.message));
-
-// database.execute("select name, age from author where id = 1");
-// database.execute("delete from author where id = 2");
-// console.log(database.execute("select name, age from author"));
-
-
-// console.log(JSON.stringify(database, undefined, " "))
+(async function () {
+     try {
+          const database = new Database();
+          await database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+          await Promise.all([
+               database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+               database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+               database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)"),
+          ]);
+          const result = await database.execute("select name, age from author");
+          console.log(JSON.stringify(result, undefined, " "));
+     } catch (e) {
+          console.log(e);
+     }
+})();
